3f3e8250d7d32316c04c1dfabc7b20ecee958ad6
oleg gugunava <oleggugunava@gmail.com>
2021-12-10 01:46:33 +0400

    Add project files.

---
+++ Shoping.Client/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿namespace Shoping.Client.Models
+{
+    public class Product
+    {
+        public string Id { get; set; }
+
+        public string Name { get; set; }   
+
+        public decimal Price { get; set; }  
+
+        public string Description { get; set; }
+
+        public string ImageFile { get; set; }  
+
+        public string Category { get; set; }
+    }
+}
